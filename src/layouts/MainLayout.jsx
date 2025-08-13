d1125f8a2d6f276cd47f3bc1c30b1b184bda439e
ten <mendezjohnmartin@gmail.com>
2025-08-13 20:17:12 +0800

    cleaned

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import Sidebar from "../components/SideBar/SideBar/SideBar";
 import "./MainLayout.css";
-import { Outlet } from "react-router-dom"; // assuming you're using React Router
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
